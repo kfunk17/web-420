eb6f8838e299212a9512310ce5f26d2bdfd8168e
Karie Funk <kfunk@my365.bellevue.edu>
2019-11-12 20:13:35 -0700

    update

--- api-gateway/models/user.js
+++ api-gateway/models/user.js
@@ -18,3 +18,14 @@ var userSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', userSchema);
+
+//user.save is used to add a new user in our database
+module.exports.add = (user, callback) => {
+    user.save(callback);
+};
+
+//getById
+module.exports.getById = (id, callback) => {
+    var query = {_id: id};
+    User.findById(query, callback);
+};
